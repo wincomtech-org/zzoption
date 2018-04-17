7087ba93e9b1e72aede7ac03536f7d1ef806dc09
guyuan1234 <37794578+guyuan1234@users.noreply.github.com>
2018-04-17 18:13:56 +0800

    222222

--- public/themes/simpleboot3/public/assets/js/information.js
+++ public/themes/simpleboot3/public/assets/js/information.js
@@ -1,17 +1,16 @@
 $(function(){
-	$(".footer>ol>a:nth-child(3)").click(function(){
-		$("html,body").animate({
-			scrollTop:"0"
-		},500);
-	});
+	// $(".footer>ol>a:nth-child(3)").click(function(){
+	// 	$("html,body").animate({
+	// 		scrollTop:"0"
+	// 	},500);
+	// });
 	var sss_s=sessionStorage.getItem("zixun");
 	$($(".fun_qh>ol>li")[sss_s]).addClass("red_text").siblings().removeClass("red_text");
 	$($(".show>div")[sss_s]).css("display","block").siblings().css("display","none");
 	$(".fun_qh>ol>li").click(function(){
 		var index_s=$(this).index();
 		sessionStorage.setItem("zixun",index_s);
-		$(this).addClass("red_text").siblings().removeClass("red_text");
-		$($(".show>div")[index_s]).css("display","block").siblings().css("display","none");
+		// $(this).addClass("red_text").siblings().removeClass("red_text");
 	});
 	var height_s=sessionStorage.getItem("bug");
 	var h=0;
