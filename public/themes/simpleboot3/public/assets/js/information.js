3a781d88e039e781cda695bb552d7d36359144ed
guyuan1234 <37794578+guyuan1234@users.noreply.github.com>
2018-04-20 16:55:58 +0800

    样式

--- public/themes/simpleboot3/public/assets/js/information.js
+++ public/themes/simpleboot3/public/assets/js/information.js
@@ -4,19 +4,19 @@ $(function(){
 	// 		scrollTop:"0"
 	// 	},500);
 	// });
-	var sss_s=sessionStorage.getItem("zixun");
-	$($(".fun_qh>ol>li")[sss_s]).addClass("red_text").siblings().removeClass("red_text");
-	$($(".show>div")[sss_s]).css("display","block").siblings().css("display","none");
-	$(".fun_qh>ol>li").click(function(){
-		var index_s=$(this).index();
-		sessionStorage.setItem("zixun",index_s);
-		// $(this).addClass("red_text").siblings().removeClass("red_text");
-	});
-	var height_s=sessionStorage.getItem("bug");
-	var h=0;
-	$(window).scrollTop(height_s);
-	$(window).scroll(function(){
-		h=$(this).scrollTop();
-		sessionStorage.setItem("bug",h);
-	});
+	// var sss_s=sessionStorage.getItem("zixun");
+	// $($(".fun_qh>ol>li")[sss_s]).addClass("red_text").siblings().removeClass("red_text");
+	// $($(".show>div")[sss_s]).css("display","block").siblings().css("display","none");
+	// $(".fun_qh>ol>li").click(function(){
+	// 	var index_s=$(this).index();
+	// 	sessionStorage.setItem("zixun",index_s);
+	// 	// $(this).addClass("red_text").siblings().removeClass("red_text");
+	// });
+	// var height_s=sessionStorage.getItem("bug");
+	// var h=0;
+	// $(window).scrollTop(height_s);
+	// $(window).scroll(function(){
+	// 	h=$(this).scrollTop();
+	// 	sessionStorage.setItem("bug",h);
+	// });
 });
